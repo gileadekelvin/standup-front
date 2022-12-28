2ec7a94c03c6a20075f6c867b407a86140a2ce00
Gileade Kelvin <gileadekelvin@gmail.com>
2022-12-28 13:58:34 -0300

    fix: fix lint

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
-    localePath: path.resolve('./public/locales')
+    localePath: path.resolve('./public/locales'),
   },
 };
